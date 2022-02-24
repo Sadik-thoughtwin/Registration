6f79d57f9665a480745bdeebf537f4ab750baaff
Sadik-thoughtwin <sadik.thoughtwin.gmail.com>
2022-02-24 17:07:31 +0530

    hcnages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Protected from "./Component/Protected";
+import Product from "./Component/Product";
+import ProductList from "./Component/ProductList";
+import Register from "./Component/Register";
+import Login from "./Component/Login";  
+import Update from "./Component/Update";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+       <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/" element={<Protected Cmp={ProductList} />}></Route>
+          <Route path="/add" element={<Protected Cmp={Product} />}></Route>
+          <Route path="/update/:id" element={<Protected Cmp={Update} />}></Route>
+        </Routes>
+      </BrowserRouter>
+    
+   </div>
+   );
 }
 
 export default App;
